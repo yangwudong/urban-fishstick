4f984011ded0582afd7ecc0ac10d01110443f12a
Jack <yangwudong@gmail.com>
2023-03-15 09:11:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='5632e5a5-5bdd-4ae7-a764-533883c04da0'
+UUID='03e2a2ee-dc4f-4235-a393-6755420e2f17'
 VMESS_WSPATH='/account'
 VLESS_WSPATH='/home'
 TROJAN_WSPATH='/trace'
